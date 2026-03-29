e5196091b091671d3c719d427d55ac1880ac88b5
Manool77 <manuel.veith@web.de>
2026-03-29 19:36:52 +0200

    Implement JsonData function for JSON output
    Added JsonData function to handle JSON responses with power data.

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 	"text/template"
 	"time"
@@ -11,42 +12,32 @@ import (
 )
 
 func Web(w http.ResponseWriter, r *http.Request) {
-
 	defaultPowerItem := map[string]kostalModbus.PowerItem{
 		"battery":     {Label: "battery", Unit: "%"},
 		"consumption": {Label: "consumption", Unit: "kW"},
 		"grid":        {Label: "to grid", Unit: "kW"},
 		"yield":       {Label: "yield", Unit: "kW"},
 	}
-
 	power, err := kostalModbus.GetPower()
 	if err != nil {
 		power = defaultPowerItem
 	}
-
 	tmpl, err := template.ParseFiles(config.Config.WebDirPath + "/frame.html")
-
-	// tmpl, err := template.New("web").Parse(html)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	if err := tmpl.Execute(w, power); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func RenderForecast(w http.ResponseWriter, r *http.Request) {
-
 	tmpl, err := template.ParseFiles(config.Config.WebDirPath + "/forecast.html")
-
-	// tmpl, err := template.New("web").Parse(html)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	now := time.Now()
 	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, config.Config.Location)
 	forecastToday, err := solcast.GetForecast(today)
@@ -54,14 +45,12 @@ func RenderForecast(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	tomorrow := today.AddDate(0, 0, 1)
 	forecastTomorrow, err := solcast.GetForecast(tomorrow)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	data := struct {
 		Today    solcast.Forecast
 		Tomorrow solcast.Forecast
@@ -69,8 +58,25 @@ func RenderForecast(w http.ResponseWriter, r *http.Request) {
 		Today:    forecastToday,
 		Tomorrow: forecastTomorrow,
 	}
-
 	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func JsonData(w http.ResponseWriter, r *http.Request) {
+	defaultPowerItem := map[string]kostalModbus.PowerItem{
+		"battery":     {Label: "battery", Unit: "%"},
+		"consumption": {Label: "consumption", Unit: "kW"},
+		"grid":        {Label: "to grid", Unit: "kW"},
+		"yield":       {Label: "yield", Unit: "kW"},
+	}
+	power, err := kostalModbus.GetPower()
+	if err != nil {
+		power = defaultPowerItem
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if err := json.NewEncoder(w).Encode(power); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
